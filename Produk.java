ecc685da983f77e50692d3081eb88c83f6a2c9e2
ritsadhil <haritsfadhil884@gmail.com>
2025-05-30 19:54:56 +0700

    record

---
+++ Produk.java
@@ -0,0 +1,31 @@
+public class Produk {
+    
+	private int harga;
+	 private String kode, nama, kategori;
+
+	public int getHarga() {
+		return harga;
+	}
+	public void setHarga(int harga) {
+		this.harga = harga;
+	}
+	public String getKode() {
+		return kode;
+	}
+	public void setKode(String kode) {
+		this.kode = kode;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getKategori() {
+		return kategori;
+	}
+	public void setKategori(String kategori) {
+		this.kategori = kategori;
+	}
+	 
+}
