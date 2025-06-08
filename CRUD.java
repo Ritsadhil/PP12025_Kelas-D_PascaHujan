463cf364e1d806925fd81e486f904959833e701f
ritsadhil <haritsfadhil884@gmail.com>
2025-06-09 00:49:43 +0700

    upd

--- CRUD.java
+++ CRUD.java
@@ -6,120 +6,123 @@ import java.util.Scanner;
 
 public class CRUD {
  public Produk[] listProduk;
-     int indexList = 0;
-	    static StringBuilder sb = new StringBuilder();
-	    static String[] dump;
-	    Scanner sc = new Scanner(System.in);
-	    static BufferedInputStream bf = null;
-	    static FileInputStream fis = null;
-	    static char eof = '.';
-	    int maxIndex = 0;
+    private Node HEAD;
+    private static final char EOF = '.';
+    private static String[] dump;
+    private static final String FILE_NAME = "Produk.txt";
+    private static StringBuilder sb = new StringBuilder();
+    private Scanner sc = new Scanner(System.in);
+    static BufferedInputStream bf = null;
+    static FileInputStream fis = null;
+    int maxIndex = 0;
 
- public void menu() {
-		    System.out.println("Menu yang tersedia : ");
-		    System.out.println("1. Tampil data ");
-		    System.out.println("2. Tambah data ");
-		    System.out.println("3. Hapus data ");
-		    System.out.println("4. Ubah data ");
-		    System.out.println("5. Cari data ");
-		    System.out.println("99. Keluar ");
-		}
-	    
-public void tulisFile() {
-		try {
-			FileWriter fw = new FileWriter("Produk.txt", false);
-			for (int a = 0; a < listProduk.length; a++) {
-				fw.write(listProduk[a].getKode() + "-" + listProduk[a].getNama() + "-" 
-						+ listProduk[a].getHarga() + "-" + listProduk[a].getKategori()
-						+ " ");
-				fw.flush();
-			}
-			fw.write(".");
-			fw.flush();
-			fw.close();
-		} catch (IOException io) {
-			System.out.println("Error IO");
-		}
-	}
+    public void menu() {
+        System.out.println("Menu yang tersedia : ");
+        System.out.println("1. Tambah data ");
+        System.out.println("2. Tampil data ");
+        System.out.println("3. Hapus data ");
+        System.out.println("4. Ubah data ");
+        System.out.println("5. Cari data ");
+        System.out.println("99. Keluar ");
+    }
 
-	    public void hitungJmlData() {
-	        int data = -1;
-	        char ch;
-	        maxIndex = 0;
-	        try {
-	            fis = new FileInputStream("Produk.txt"); // Buka file
-	            bf = new BufferedInputStream(fis);
+     public void tulisFile() {
+        try {
+            FileWriter fw = new FileWriter("Produk.txt", false);
+            Node currNode = HEAD;
+            while (currNode != null) {
+                Produk p = currNode.getData();
+                fw.write(p.getKode() + "-" + p.getNama() + "-" + 
+                        p.getHarga() + "-" + p.getKategori() + " ");
+                        currNode = currNode.getNext();
+            }
+            fw.write(".");
+            fw.close();
+        } catch (IOException io) {
+            System.out.println("Error IO");
+        }
+    }
 
-	            if (bf != null) {
-	                data = bf.read();
-	                ch = (char) data;
-	                while (data != -1 && ch != eof) { // Baca hingga akhir file
-	                    if (ch == ' ') {
-	                        maxIndex++;
-	                    }
-	                    data = bf.read();
-	                    ch = (char) data;
-	                }
-	            }
-	            dump = new String[maxIndex];
-	            Produk p = new Produk();
-	            bf.close();
-	        } catch (IOException e) {
-	            System.out.println("Error I/O");
-	        }
-	    }
+    public void hitungJmlData() {
+        int data;
+        char ch;
+        maxIndex = 0;
 
-	    public static void bacaFile() {
-	        int data = -1;
-	        char ch;
-	        int index = 0;
-	        try {
-	            fis = new FileInputStream("Produk.txt");
-	            bf = new BufferedInputStream(fis);
-	            if (bf != null) {
-	                data = bf.read();
-	                ch = (char) data;
-	                while (data != -1 && ch != eof) {
-	                    if (ch != ' ') {
-	                        sb.append(ch);
-	                    } else {
-	                        dump[index] = sb.toString();
-	                        index++;
-	                        sb = new StringBuilder();
-	                    }
-	                    data = bf.read();
-	                    ch = (char) data;
-	                }
-	            }
-	            bf.close();
-	        } catch (IOException e) {
-	            System.out.println("Error I/O");
-	        }
-	    }
+        try {
+            fis = new FileInputStream(FILE_NAME);
+            bf = new BufferedInputStream(fis);
 
-	    public void ambilData() {
-	        hitungJmlData();
-	        bacaFile();
-	        System.out.println("=============== Ambil Data Mahasiswa ===============");
-	        String[] temp;
+            while ((data = bf.read()) != -1 && (ch = (char) data) != EOF) {
+                if (ch == ' ') {
+                    maxIndex++;
+                }
+            }
 
-	        for (int a = 0; a < dump.length; a++) {
-	            if (dump[a] != null && dump[a].contains("-")) { 
-	                temp = dump[a].split("-"); 
-	                if (temp.length == 4) { 
-	                    listProduk[a] = new Produk();
-	                    listProduk[a].setKode(temp[0].trim()); 
-	                    listProduk[a].setNama(temp[1].trim());                
-	                    listProduk[a].setKategori(temp[2].trim());         
-	                    listProduk[a].setHarga(Integer.parseInt(temp[3].trim()));           
-	                } else {
-	                    System.out.println("Format data tidak valid pada indeks " + a + ": " + dump[a]);
-	                }
-	            } else {
-	                System.out.println("Data kosong atau tidak sesuai format pada indeks " + a);
-	            }
-	        }
-	        System.out.println("=============== Data Berhasil Diambil ===============");
-	    }
+            dump = new String[maxIndex];
+        } catch (IOException e) {
+            System.out.println("Error membaca file: " + e.getMessage());
+        } finally {
+            try {
+                if (bf != null) bf.close();
+                if (fis != null) fis.close();
+            } catch (IOException e) {
+                System.out.println("Gagal menutup file: " + e.getMessage());
+            }
+        }
+    }
+    public static void bacaFile() {
+        int data;
+        char ch;
+        int index = 0;
+        sb = new StringBuilder();
+
+        try {
+            fis = new FileInputStream(FILE_NAME);
+            bf = new BufferedInputStream(fis);
+
+            while ((data = bf.read()) != -1 && (ch = (char) data) != EOF) {
+                if (ch != ' ') {
+                    sb.append(ch);
+                } else {
+                    dump[index++] = sb.toString();
+                    sb = new StringBuilder();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error membaca file: " + e.getMessage());
+        } finally {
+            try {
+                if (bf != null) bf.close();
+                if (fis != null) fis.close();
+            } catch (IOException e) {
+                System.out.println("Gagal menutup file: " + e.getMessage());
+            }
+        }
+    }
+    public void ambilData() {
+        hitungJmlData();
+        bacaFile();
+        HEAD = null;
+
+        System.out.println("=============== Ambil Data Produk ===============");
+        for (String item : dump) {
+            if (item != null && item.contains("-")) {
+                String[] temp = item.split("-");
+                if (temp.length == 4) {
+                    Produk p = new Produk();
+                    p.setKode(temp[0].trim());
+                    p.setNama(temp[1].trim());
+                    p.setHarga(Integer.parseInt(temp[2].trim()));
+                    p.setKategori(temp[3].trim());
+                    addTail(p);
+                }
+            }
+        }
+        System.out.println("=============== Data Berhasil Diambil ===============");
+    }
+
+ 
+
+   
 
 }
